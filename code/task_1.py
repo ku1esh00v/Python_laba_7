93177b3f08a680ebad1d74e0bb578cb078638b8a
Oleg Kuleshov <144348564+ku1esh00v@users.noreply.github.com>
2023-12-12 19:29:21 +0300

    воспользовался функцией prod и списковым включением

--- code/task_1.py
+++ code/task_1.py
@@ -2,16 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from math import prod
 
-if __name__ == '__main__':
+if __name__ == 'main':
     A = list(map(int, input().split()))
     if len(A) != 10:
         print('Неверный размер списка', file=sys.stderr)
         exit(1)
 
-    total = 1
-    for item in A:
-        if item < 0:
-            total *= item
-
+    total = prod(item for item in A if item < 0)
     print(total)
